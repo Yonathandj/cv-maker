46774c2d479e037b063884d37cf21c87b4be6b66
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-08-28 16:15:59 +0700

    Change icon add edu

--- frontend/src/components/education/EducationList.jsx
+++ frontend/src/components/education/EducationList.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import { FaPlus } from "react-icons/fa";
+import { FaPlusCircle } from "react-icons/fa";
 
 import EducationListItem from "./EducationListItem";
 
 const EducationList = ({ setSectionName }) => {
   return (
     <React.Fragment>
-        <section>
-          <EducationListItem />
-          <button
-            className="flex gap-2 items-center mt-6 mx-auto px-10 py-2 bg-slate-300 rounded-md"
-            onClick={() => {
-              setSectionName("form");
-            }}
-          >
-            <FaPlus />
-            <h4 className="text-base font-semibold">Add</h4>
-          </button>
-        </section>
+      <section>
+        <EducationListItem />
+        <button
+          className="flex gap-2 items-center mt-6 mx-auto px-10 py-2 bg-slate-300 rounded-md"
+          onClick={() => {
+            setSectionName("form");
+          }}
+        >
+          <FaPlusCircle />
+          <h4 className="text-base font-semibold">Add</h4>
+        </button>
+      </section>
     </React.Fragment>
-  )
+  );
 };
 
 export default EducationList;
