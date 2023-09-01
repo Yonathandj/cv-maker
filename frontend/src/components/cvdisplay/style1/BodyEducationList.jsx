66df45b2da9b0ca4579cf15dbba63327ca066b81
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-09-01 13:04:32 +0700

    Display experience

--- frontend/src/components/cvdisplay/style1/BodyEducationList.jsx
+++ frontend/src/components/cvdisplay/style1/BodyEducationList.jsx
@@ -6,14 +6,14 @@ const BodyEducationList = ({ educations }) => {
       {educations.map(
         (education) =>
           education.isShow && (
-            <section key={education.id} className="flex gap-6 items-center p-2">
-              <div>
+            <section key={education.id} className="flex items-center p-4">
+              <div className="w-1/4">
                 <p className="text-base font-medium">
                   {education.startDate + " / " + education.endDate}
                 </p>
                 <p className="text-base font-medium">{education.location}</p>
               </div>
-              <div>
+              <div className="w-3/4">
                 <p className="text-xl font-medium">{education.school}</p>
                 <p className="text-base font-semibold">{education.degree}</p>
               </div>
--- frontend/src/components/cvdisplay/style1/BodyExperienceList.jsx
+++ frontend/src/components/cvdisplay/style1/BodyExperienceList.jsx
@@ -1,5 +1,32 @@
-const BodyExperienceList = () => {
-  return <div></div>;
+import React from "react";
+
+const BodyExperienceList = ({ experiences }) => {
+  return (
+    <React.Fragment>
+      {experiences.map(
+        (experience) =>
+          experience.isShow && (
+            <section key={experience.id} className="flex items-center p-4">
+              <div className="w-1/4">
+                <p className="text-base font-medium">
+                  {experience.startDate + " / " + experience.endDate}
+                </p>
+                <p className="text-base font-medium">{experience.location}</p>
+              </div>
+              <div className="w-3/4 ml-5">
+                <p className="text-xl font-medium">{experience.companyName}</p>
+                <p className="text-base font-semibold">
+                  {experience.positionTitle}
+                </p>
+                <p className="text-base font-semibold">
+                  {experience.description}
+                </p>
+              </div>
+            </section>
+          )
+      )}
+    </React.Fragment>
+  );
 };
 
 export default BodyExperienceList;
